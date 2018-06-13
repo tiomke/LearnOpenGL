df993a4cfa39a49bdf254c26ee12b2ec6549799c
tangyikejun <tangyikejun@163.com>
2018-06-13 15:14:49 +0800

    修正法线基于局部坐标而不是世界坐标的问题。

--- First/First/Shader/vshader.glsl
+++ First/First/Shader/vshader.glsl
@@ -13,6 +13,7 @@ void main()
 	// 设置顶点着色器的输出
 	gl_Position = proj * view * model * vec4(aPos, 1.0);
 	// 注：实际工程中的程序，输入数据往往不是标准设备坐标，需要先转换为标准设备坐标
-	Normal = aNormal;
+	Normal = mat3(transpose(inverse(model)))*aNormal; // mat3(transpose(inverse(model))) 是法线矩阵，mat3 把矩阵转换成 3×3 矩阵，去除位移属性。 法线矩阵的作用是把局部坐标下的法向量变换成世界坐标的法向量
+	// 更好的方式是在cpu计算好法线矩阵再传进来，因为写在这里，每个片段都要做一遍计算，而进行逆矩阵运算开销是比较大的。
 	FragPos = vec3(model * vec4(aPos, 1.0)); // 把局部坐标转换成世界坐标
 }
